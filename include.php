40051924f98494e4ca2f5eaafdd2afa4d9dc257b
Achuth Nair <98337043+achuthnair@users.noreply.github.com>
2023-02-10 01:31:23 -0500

    Update include.php

--- include.php
+++ include.php
@@ -1,7 +1,7 @@
 <?php
 
-    define("DB_SERVER", "127.0.0.1:3306");
-    define("DB_USER", "root");
-    define("DB_PASS", "achuthnair");
+    define("DB_SERVER", "");
+    define("DB_USER", "");
+    define("DB_PASS", "");
     define("DB_NAME", "icd_codes")
 ?>
